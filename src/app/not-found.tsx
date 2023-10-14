6aeb6422b79727f24bd7eae9cf0eb7b28624a505
schatzen <gomieti@naver.com>
2023-10-14 23:29:47 +0900

    Not Found Page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,3 @@
+export default function NotFoundPage() {
+  return <h1>Can't find page😘</h1>;
+}
---
+++ src/app/products/not-found.tsx
@@ -0,0 +1,3 @@
+export default function ProductsNotFountPage() {
+  return <h1>페이지를 찾을 수 없음</h1>;
+}
